c9be2825d1e65ee146a74cc6a093bb37e72df1a7
Michael Kammer <michael.kammer@mailbox.org>
2023-01-02 14:44:35 +0100

    DOC: documented coef method

--- R/coef.mfpa.R
+++ R/coef.mfpa.R
@@ -1,3 +1,15 @@
+#' Extract coefficients from a `mfpa` object 
+#' 
+#' This function is a method for the generic [stats::coef()] function for objects 
+#' of class `mfpa`. 
+#' 
+#' @param object an object of class `mfpa`, usually, a result of a call to
+#' [mfpa()].
+#' 
+#' @return 
+#' Named numeric vector of coefficients extracted from the model `object`.
+#' 
+#' @export
 coef.mfpa <- function(x) {
   x$coefficients
 }

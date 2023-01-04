7e3b211afc07e2808aa6fbf71ff95a7fa3a9c3d3
Michael Kammer <michael.kammer@mailbox.org>
2023-01-04 15:09:25 +0100

    REF: moved and renamed find_best_model_fp1

--- R/find_best_fp_step.R
+++ R/find_best_fp_step.R
@@ -1,4 +1,4 @@
-#' Function to estimate the best FP functions for a variable
+#' Function to estimate the best FP functions for a single variable
 #' 
 #' See [mfpa()] for a brief summary on the notation used here. 
 #' 
@@ -6,13 +6,32 @@
 #' The function selection procedure (FSP) is used if the p-value criterion is 
 #' chosen, whereas the criteria AIC and BIC select the model with the smallest 
 #' AIC and BIC, respectively.
-find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
-                            criterion, select, init, alpha, keep, powers, method, strata,
-                            ftest, control, rownames, nocenter, verbose, acdx) {
+find_best_fp_step <- function(x,
+                              y, 
+                              xi,
+                              allpowers, 
+                              df, 
+                              weights, 
+                              offset, 
+                              family,
+                              criterion, 
+                              select, 
+                              init, 
+                              alpha, 
+                              keep, 
+                              powers, 
+                              method, 
+                              strata,
+                              ftest, 
+                              control, 
+                              rownames, 
+                              nocenter, 
+                              verbose, 
+                              acdx) {
   N <- dim(x)[1L]
   # If df = 1 then we just fit usual linear models and test: NULL vs Linear
   if (df == 1) {
-    fits <- find_best_fp_step1(
+    fits <- find_best_fp1_step(
       x = x, y = y, xi = xi, allpowers = allpowers,
       weights = weights, offset = offset, family = family,
       criterion = criterion, select = select, alpha = alpha,
@@ -102,21 +121,21 @@ find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
         }
       } else {
         best.fp.power <- switch(criterion,
-          "AIC" = bestfuns[[2]][[index.bestmodel]],
-          "BIC" = bestfuns[[3]][[index.bestmodel]]
+                                "AIC" = bestfuns[[2]][[index.bestmodel]],
+                                "BIC" = bestfuns[[3]][[index.bestmodel]]
         )
       }
       # Printing on the screen
       if (verbose) {
         if (criterion == "pvalue") {
           if (ftest) {
-              print_mfp_summary_2(
+            print_mfp_summary_2(
               namex = xi, dev.all = dev.roy.all, df.res = df.all, dev.diff = dev.diff, f = fstatistic,
               df.den = df.all, pvalues = pvalue, best.function = bestfuns[[5]],
               index.bestmodel = index.bestmodel, acd = T
             )
           } else {
-              print_mfp_summary_1(
+            print_mfp_summary_1(
               namex = xi,
               dev.all = dev.all,
               dev.diff = dev.diff,
@@ -127,8 +146,8 @@ find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
           }
         } else {
           switch(criterion,
-            "AIC" = print_mfp_summary_3(xi, gic = aic.all, keep = keep, best.function = bestfuns[[2]], acd = T),
-            "BIC" = print_mfp_summary_3(xi, gic = bic.all, keep = keep, best.function = bestfuns[[3]], acd = T)
+                 "AIC" = print_mfp_summary_3(xi, gic = aic.all, keep = keep, best.function = bestfuns[[2]], acd = T),
+                 "BIC" = print_mfp_summary_3(xi, gic = bic.all, keep = keep, best.function = bestfuns[[3]], acd = T)
           )
         }
       }
@@ -253,23 +272,23 @@ find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
         best.fp.power <- 1 # Linear function
       } else if (index.bestmodel == 3) { # best FP1 function
         best.fp.power <- switch(criterion,
-          "pvalue" = if (ftest) {
-            bestfp1x[5]
-          } else {
-            bestfp1x[1]
-          },
-          "AIC" = bestfp1x[2],
-          "BIC" = bestfp1x[3]
+                                "pvalue" = if (ftest) {
+                                  bestfp1x[5]
+                                } else {
+                                  bestfp1x[1]
+                                },
+                                "AIC" = bestfp1x[2],
+                                "BIC" = bestfp1x[3]
         )
       } else { # best FPm function
         best.fp.power <- switch(criterion,
-          "pvalue" = if (ftest) {
-            bestfp.dev.roy[[index.bestmodel - 2]]
-          } else {
-            bestfp.dev[[index.bestmodel - 2]]
-          }, # bestfp.dev = c(FP1, FP2,...) SO subtract 2 to get correct index in bestfp.dev
-          "AIC" = bestfp.aic[[index.bestmodel - 2]], # e.g FP2 = index.model = 4 but it is in position 2 in bestfp.dev
-          "BIC" = bestfp.bic[[index.bestmodel - 2]]
+                                "pvalue" = if (ftest) {
+                                  bestfp.dev.roy[[index.bestmodel - 2]]
+                                } else {
+                                  bestfp.dev[[index.bestmodel - 2]]
+                                }, # bestfp.dev = c(FP1, FP2,...) SO subtract 2 to get correct index in bestfp.dev
+                                "AIC" = bestfp.aic[[index.bestmodel - 2]], # e.g FP2 = index.model = 4 but it is in position 2 in bestfp.dev
+                                "BIC" = bestfp.bic[[index.bestmodel - 2]]
         )
       }
       # Print results if verbose = T
@@ -289,13 +308,13 @@ find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
             }, list(1), 0)
           }
           if (ftest) {
-              print_mfp_summary_2(
+            print_mfp_summary_2(
               namex = xi, dev.all = dev.roy.all, df.res = df.all, dev.diff = dev.diff, f = fstatistic,
               df.den = df.all, pvalues = pvalue, best.function = best.function1,
               index.bestmodel = index.bestmodel, acd = F
             )
           } else {
-              print_mfp_summary_1(
+            print_mfp_summary_1(
               namex = xi, dev.all = dev.all, dev.diff = dev.diff,
               pvalues = pvalue, index.bestmodel = index.bestmodel,
               best.function = best.function1, acd = F
@@ -314,8 +333,8 @@ find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
             append(bestfp.bic, list(1), 0)
           }
           switch(criterion,
-            "AIC" = print_mfp_summary_3(xi, gic = aic.all, keep = keep, best.function = best.function.aic, acd = F),
-            "BIC" = print_mfp_summary_3(xi, gic = bic.all, keep = keep, best.function = best.function.bic, acd = F)
+                 "AIC" = print_mfp_summary_3(xi, gic = aic.all, keep = keep, best.function = best.function.aic, acd = F),
+                 "BIC" = print_mfp_summary_3(xi, gic = bic.all, keep = keep, best.function = best.function.bic, acd = F)
           )
         }
       }
@@ -340,6 +359,158 @@ find_best_fp_step <- function(x, y, xi, allpowers, df, weights, offset, family,
       }
     )
   }
+  return(fit)
+}
+
+#' Helper to select between null and linear model for a single variable
+#' 
+#' To be used in [find_best_fp_step()]. Only used if df = 1 for a variable.
+find_best_fp1_step <- function(x, 
+                               y, 
+                               xi, 
+                               allpowers, 
+                               df, 
+                               weights, 
+                               offset, 
+                               control,
+                               family, 
+                               criterion, 
+                               select, 
+                               alpha, 
+                               keep, 
+                               powers, 
+                               method,
+                               strata, 
+                               ftest, 
+                               rownames, 
+                               nocenter, 
+                               verbose, 
+                               acdx) {
+  #  Set df = 1 in the extract_adjustment_data() because linearity is assumed and fpdata
+  # would be original x of interest (untransformed)
+  xadjv <- extract_adjustment_data(
+    x = x, xi = xi, allpowers = allpowers, df = 1,
+    powers = powers, acdx = acdx
+  ) # degree, s and scale does not play any role here
+  # Number of observations and a quantity for BIC calculation
+  N <- dim(x)[1L]
+  logn <- log(N)
+  # Fit a null model, which is a model that has no x of interest but only adjustment variables.
+  modelnull <- fit_model(
+    x = xadjv$adjdata, y = y, family = family, weights = weights,
+    offset = offset, method = method, strata = strata,
+    control = control, rownames = rownames,
+    nocenter = nocenter
+  )
+  # The total number of parameters in the model. The scale parameter is included
+  # in Gaussian. FP powers in the models is not considered since its not estimated
+  dfnull <- modelnull$df
+  # Deviance, AIC, BIC and SSE for a null model
+  devnull <- -2 * modelnull$logl
+  aic.null <- devnull + 2 * dfnull
+  bic.null <- devnull + logn * dfnull
+  sse.null <- modelnull$SSE
+  dev.roy.null <- deviance_gaussian(RSS = sse.null, weights = weights, n = N)
+  # Residual degrees of freedom. It includes scale parameter. for consistency
+  # with stata we get rid of it
+  df.null.res <- N - (dfnull - 1)
+  # Fit a model based on the assumption that x is linear while accounting for other variables.
+  xx <- cbind(xadjv$fpdata, xadjv$adjdata)
+  colnames(xx) <- c("xnew", colnames(xadjv$adjdata))
+  modellinear <- fit_model(
+    x = xx, y = y, family = family, weights = weights,
+    offset = offset, method = method, strata = strata,
+    control = control, rownames = rownames,
+    nocenter = nocenter
+  )
+  # Deviance, AIC, BIC and SSE for a linear model
+  devlin <- -2 * modellinear$logl
+  dflin <- modellinear$df # includes scale parameter, which is fine since glm uses it in aic or bic calculation
+  aic.lin <- devlin + 2 * dflin
+  bic.lin <- devlin + logn * dflin
+  sse.linear <- modellinear$SSE
+  dev.roy.lin <- deviance_gaussian(RSS = sse.linear, weights = weights, n = N)
+  # Residual degrees of freedom. It includes scale parameter. for consistency
+  # with stata mfpa results we get rid of it
+  df.linear.res <- N - (dflin - 1)
+  # Combine the deviance, AIC,BIC and SSE for a null and linear model
+  dev.all <- c(devnull, devlin)
+  dev.roy.all <- c(dev.roy.null, dev.roy.lin)
+  aic.all <- c(aic.null, aic.lin)
+  bic.all <- c(bic.null, bic.lin)
+  sse.all <- c(sse.null, sse.linear)
+  df.all <- c(df.null.res, df.linear.res)
+  names(dev.all) <- names(dev.roy.all) <- names(aic.all) <- names(bic.all) <- names(sse.all) <- names(df.all) <- c("null", "linear")
+  # Deviance difference
+  dev.diff <- as.numeric(devnull - devlin)
+  # Model selection based on AIC, BIC or P-values
+  if (criterion == "AIC") {
+    index.bestmodel <- which.min(aic.all) # 1 = variable removed, 2 = linear
+    # keep xi in the model
+    if (xi %in% keep) {
+      index.bestmodel <- which.min(aic.all[-1]) + 1
+    }
+  } else if (criterion == "BIC") {
+    index.bestmodel <- which.min(bic.all)
+    if (xi %in% keep) {
+      index.bestmodel <- which.min(bic.all[-1]) + 1
+    }
+  } else { # criterion=="pvalue"
+    if (ftest) {
+      stats <- calculcate_f_statistic_stata(
+        dev_reduced = dev.roy.all[1], dev_full = dev.roy.all[2], d1 = 1,
+        d2 = df.all[2], n = N
+      )
+      pvalue <- stats$pval
+      fstatistic <- stats$fstatistic
+      dev.diff <- stats$dev.diff
+    } else {
+      pvalue <- pchisq(q = dev.diff, df = dflin - dfnull, lower.tail = F)
+    }
+    names(pvalue) <- names(dev.diff) <- c("Null vs Linear")
+    index.bestmodel <- ifelse(pvalue > select, 1, 2)
+  }
+  if (verbose) {
+    if (criterion == "pvalue") {
+      if (ftest) {
+        print_mfp_summary_2(
+          namex = xi, dev.all = dev.roy.all, df.res = df.all, df.den = df.all, dev.diff = dev.diff, f = fstatistic,
+          pvalues = pvalue, best.function = list(1), index.bestmodel = index.bestmodel, acd = acdx[xi]
+        ) # acdx[xi]
+      } else {
+        print_mfp_summary_1(
+          namex = xi, dev.all = dev.all, dev.diff = dev.diff,
+          pvalues = pvalue,
+          index.bestmodel = index.bestmodel,
+          best.function = list(1), acd = acdx[xi]
+        ) # acdx[xi]
+      }
+    } else {
+      switch(criterion,
+             "AIC" = print_mfp_summary_3(xi, gic = aic.all, keep = keep, best.function = list(1), acd = F),
+             "BIC" = print_mfp_summary_3(xi, gic = bic.all, keep = keep, best.function = list(1), acd = F)
+      )
+    }
+  }
+  
+  fit <- list(
+    overall.best.fn = as.numeric(ifelse(index.bestmodel == 1, NA, 1)),
+    dev.all = if (ftest) {
+      dev.roy.all
+    } else {
+      dev.all
+    }, aic.all = aic.all,
+    bic.all = bic.all, sse.all = sse.all,
+    index.bestmodel = as.numeric(index.bestmodel),
+    dev.diff = dev.diff,
+    pvalues = if (criterion == "pvalue") {
+      pvalue
+    } else {
+      NA
+    },
+    fstatistic = ifelse(ftest && criterion == "pvalue", fstatistic, NA),
+    df.all = df.all
+  )
   return(fit)
 }
 

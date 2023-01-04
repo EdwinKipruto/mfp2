7930891915d0c654128afd184796a64119017712
Michael Kammer <michael.kammer@mailbox.org>
2023-01-04 16:41:13 +0100

    REF: refactored generate_fp_powers

--- R/generate_fp_powers.R
+++ R/generate_fp_powers.R
@@ -2,23 +2,24 @@
 #'
 #' @param degree The degree of fractional polynomial; degree = 1 is FP1 and
 #' returns 8 powers; degree 2 is FP2 and returns 36 pairs of powers; degree 3 is
-#'  FP3 and returns 120 triple of powers etc.
-#' @param powers the set of fractional polynomial. Default is NULL in which the set
-#' s = {-2, -1, -0.5, 0, 0.5, 1, 2, 3} is used
-#' @return A matrix of powers depending on the degree
+#'  FP3 and returns 120 triples of powers etc.
+#' @param powers the set of allowed powers for the fractional polynomials. 
+#' Default is `NULL` and the set {-2, -1, -0.5, 0, 0.5, 1, 2, 3} is used.
+#' 
+#' @return 
+#' A matrix of powers with degree columns and rows depending on the degree.
+#' 
 #' @importFrom arrangements  combinations
 #' @export
-generate_fp_powers <- function(degree = NULL, powers = NULL) {
-  # set FP2 as the default maximum degree permitted
+generate_fp_powers <- function(degree = NULL, 
+                               powers = NULL) {
   if (is.null(degree)) {
     degree <- 2
   }
-  # set default FP set when not provided
   if (is.null(powers)) {
     powers <- c(-2, -1, -0.5, 0, 0.5, 1, 2, 3)
   }
-  # Find all possible combination of set s. Here we use combinations with
-  # repetition because we allow powers to be repeated e.g (0,0) or (1,1) etc
-  all_powers <- arrangements::combinations(x = powers, k = degree, replace = T)
-  return(all_powers)
+  
+  # using replacement because powers may be repeated e.g. (0,0) or (1,1)
+  arrangements::combinations(x = powers, k = degree, replace = TRUE)
 }

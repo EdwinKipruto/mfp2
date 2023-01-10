cf48b32fb73fab58cd0ab1dd6b6c584814ec76e6
Michael Kammer <michael.kammer@mailbox.org>
2023-01-10 11:48:31 +0100

    REF: reworked fp transformation of vectors

--- R/transformations_vector.R
+++ R/transformations_vector.R
@@ -5,12 +5,30 @@
 #' for a variable.
 #' 
 #' @details 
-#' For example, this function  generates new variables for x given the power(s)
-#' e.g., xnew = log(x) when power = 0 and scale = F and center = F.
+#' The fp transformation generally transforms `x` as follows. For each pi in
+#' `power` = (p1, p2, ..., pn) it creates a variable x^pi and returns the
+#' collection of variables as a matrix. It may process the data using 
+#' shifting, scaling and centering as desired. 
+#' 
+#' A special case are repeated powers, i.e. when some pi = pj. In this case, 
+#' the fp transformations are given by x^pi and x^pi * log(x). In case
+#' more than 2 powers are repeated they are repeatedly multiplied with 
+#' log(x) terms, e.g. pi = pj = pk leads to x^pi, x^pi * log(x) and 
+#' x^pi * log(x)^2. 
+#' 
+#' Binary variables are not transformed, but may only be centered. 
+#' 
+#' @section Data processing: 
+#' An important note on data processing. Variables are shifted and scaled 
+#' before being transformed by any powers. That is to ensure positive values
+#' and reasonable scales. However, they are centered after transformation. 
+#' That is to ensure that the correlation between variables stay intact, 
+#' as centering before transformation would affect them. This is described
+#' in Sauerbrei et al (2006), as well as in the Stata manual of `mfp`.
 #' 
 #' @param x a vector of a predictor variable.
-#' @param power The user-supplied FP power. Default is 1 (linear). Must be a 
-#' vector of length 2 for acd transformation.
+#' @param power a numeric vector indicating the FP power. Default is 1 (linear). 
+#' Must be a vector of length 2 for acd transformation.
 #' @param scale scaling factors for x of interest. Must be positive integers.
 #' Default is `NULL` and scaling factors are automatically estimated by the
 #' program. If no scaling is needed set `scale = 1`.
@@ -26,7 +44,13 @@
 #' @return 
 #' Returns a matrix of transformed variable(s). The number of columns
 #' depends on the number of powers provided, the number of rows is equal to the
-#' length of `x`.
+#' length of `x`. If all powers are `NA`, then this function returns `NULL`.
+#' 
+#' @references 
+#' Sauerbrei, W., Meier-Hirmer, C., Benner, A. and Royston, P., 2006. 
+#' \emph{Multivariable regression model building by using fractional 
+#' polynomials: Description of SAS, STATA and R programs. 
+#' Comput Stat Data Anal, 50(12): 3464-85.}
 #' 
 #' @export
 transform_fp_vector <- function(x, 
@@ -34,104 +58,101 @@ transform_fp_vector <- function(x,
                                 scale = NULL, 
                                 center = FALSE, 
                                 shift = NULL) {
-  # number of observations
-  N <- length(x)
-  if (all(is.na(power))) { # important because omitted variables have power of NA
-    x <- NULL
-    xcenter <- NULL
-  } else {
-    # Do not transform x when it is a two level variable but center if necessary
-    nx <- length(unique(x))
-    if (nx <= 2) {
-      # warning(paste0("x has 2 different values, original x ",if(center){paste0("(centered by min(x))")}, " values returned"))
-      x <- as.matrix(x)
-      # center using the lower(minimum) of the two distinct values of the covariates
-      # as also done in stata mfp by Patrick Royston
-      if (center) {
-        xcenter <- min(x)
-        x <- x - xcenter
-      }
-    } else {
-      # shift x
-      if (is.null(shift)) {
-        shift <- find_shift_factor(x)
-      }
-      x <- x + shift
-      # scale x
-      if (is.null(scale)) {
-        scale <- find_scale_factor(x)
-      }
-      x <- x / scale
-      # sort the powers such that 1,0,1 is equal to 0,1,1. p1<p2<....<pm. see
-      # Royston and Altman 1994 near equation 6 for explanation
-      power <- sort(power) # if NA exist it will be removed
-      # create a list that will hold the transformed variables
-      H <- vector(mode = "list", length = length(power))
-      # Deal with the first power
-      H[[1]] <- ifelse(power[1] == rep(0, N), log(x), x^power[1])
-      # check the length of the power. If length >=2 use a loop
-      np <- length(power)
-      # Deal with the remaining powers
-      if (np > 1) {
-        # check whether all powers are identical
-        nu <- length(unique(power))
-        # all powers are repeated
-        if (nu == 1) {
-          for (j in seq_len(np - 1)) {
-            k <- j + 1
-            H[[k]] <- H[[j]] * log(x)
-          }
-        } else {
-          # Either a mix of powers like 1,2,2 or 1,2,3
-          for (j in seq_len(np - 1)) { # check whether while loop is possible
-            k <- j + 1
-            # the subsequent power is repeated
-            if (power[k] == power[j]) {
-              H[[k]] <- H[[j]] * log(x)
-              # the subsequent powers are not repeated e.g 1,2,4. so we are dealing with say 2,4
-            } else {
-              H[[k]] <- ifelse(power[k] == rep(0, N), log(x), x^power[k])
-            }
-          }
-        }
-      }
-      # combine the transformed variables into a matrix
-      x <- do.call(cbind, H)
-      # center each transformed variable using: f(x) - mean(f(x))
-      if (center) {
-        x <- scale(x, scale = F)
-      }
-    }
+  
+  if (all(is.na(power))) { 
+    # variable omitted
+    return(NULL)
   }
   
-  x
+  # do not transform x when it is a two level variable but center if necessary
+  if (length(unique(x)) <= 2) {
+    x <- as.matrix(x)
+    # center using the lower(minimum) of the two distinct values of the covariates
+    # as also done in stata mfp by Patrick Royston
+    if (center) {
+      return(x - min(x))
+    } else return(x)
+  } 
+  
+  # process input data by shifting and scaling
+  if (is.null(shift)) {
+    shift <- find_shift_factor(x)
+  }
+  
+  if (is.null(scale)) {
+    scale <- find_scale_factor(x)
+  }
+  
+  x <- x + shift
+  x <- x / scale
+  
+  # sort the powers
+  # see Royston and Altman 1994 near equation 6 for explanation
+  # note that sort removes NAs 
+  power <- sort(power)
+
+  # transform data
+  x_trafo <- matrix(NA, nrow = length(x), ncol = length(power))
+  # transform using first power
+  x_trafo[, 1] <- transform_power_vector(x, power[1])
+  # transform other powers via loop if necessary
+  for (j in seq_len(length(power) - 1)) { 
+    k <- j + 1
+    if (power[k] == power[j]) {
+      # the subsequent power is repeated, e.g. 1, 2, 2
+      # repeatedly multiply with log(x), thereby creating powers of log(x)
+      x_trafo[, k] <- x_trafo[, j] * log(x)
+    } else {
+      # the subsequent power is not repeated, e.g. 1, 2, 3
+      x_trafo[, k] <- transform_power_vector(x, power[k])
+    }
+  }
+
+  if (center) {
+    x_trafo <- scale(x_trafo, scale = FALSE)
+  }
+  
+  x_trafo
 }
 
 #' @describeIn transform_fp_vector Function to generate acd transformation.
+#' @export
 transform_acd_vector <- function(x, 
                                  power = c(1, 1),
                                  shift = NULL, 
                                  s = NULL, 
                                  scale = NULL, 
                                  center = FALSE) {
-  # the length of powers must be equal to 2
-  np <- length(power)
-  if (np != 2) stop("The length of powers supplied is ", np, ". Two powers are needed")
+
+  
+  if (length(power) != 2) 
+    stop("! power must be of length two.", 
+         sprintf("i The length of powers supplied is %d.", length(power)))
+  
   if (all(is.na(power))) {
-    xx <- NULL
-  } else {
-    # Transform x and acdx using the supplied powers
-    # Approximate cumulative distribution (ACD)
-    xa <- acd(x, power = NULL, shift = shift, s = s, scale = scale)$acd
-    xa <- transform_fp_vector(x = xa, power = power[2],
-                              scale = scale, shift = shift, 
-                              center = center)
-    x2 <- transform_fp_vector(x = x, power = power[1],
+    return(NULL)
+  } 
+  
+  # transform x and acdx using the supplied powers
+  x_acd <- acd(x, power = NULL, shift = shift, s = s, scale = scale)$acd
+  x_acd <- transform_fp_vector(x = x_acd, power = power[2],
+                               scale = scale, shift = shift, 
+                               center = center)
+  x_fp <- transform_fp_vector(x = x, power = power[1],
                               scale = scale, shift = shift,
                               center = center)
-    # return a matrix of two variables
-    xx <- cbind(x2, xa)
-  }
+
+  cbind(x_fp, x_acd)
+}
+
+#' Simple function to transform vector by a single power
+#' 
+#' @param x a vector of a predictor variable.
+#' @param power single power. 
+transform_power_vector <- function(x,
+                                   power = 1) {
+  if (power == 0)
+    return(log(x))
   
-  xx
+  x^power
 }

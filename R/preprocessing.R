f1204ed6d8d7faa47f7a718b0f8b73e870910e44
Michael Kammer <michael.kammer@mailbox.org>
2023-01-10 12:45:30 +0100

    REF: reworked input processing
    In particular, for binary variables no shifting and scaling is performed anymore.

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,37 +1,78 @@
-#' A function that calculates an integer used to scale x. Estimation of powers
-#' is unaffected by scaling, the same powers are found for x/scale. In extreme
-#' cases scaling is necessary to preserve accuracy (Royston and Sauerbrei 2008).
-#' We used the formula in section 4.11.1 of Royston and Sauerbrei (2008) book
+#' Function that calculates an integer used to scale predictor
+#' 
+#' @details
+#' For details on why scaling is useful see the corresponding section in the
+#' documentation of [mfpa()].
+#' 
+#' Note that the estimation of powers is unaffected by scaling, the same powers 
+#' are found for scaled input data. In extreme cases scaling is necessary to 
+#' preserve accuracy, see Royston and Sauerbrei (2008).
+#' This formula uses the scaling formula from Section 4.11.1 of 
+#' Royston and Sauerbrei (2008). Further information can also be found in the 
+#' Stata manual for mfp at https://www.stata.com/manuals/rfp.pdf.
 #'
-#' @param x a vector of predictor variable where x is already shifted to positive values.
-#' @return An integer
+#' @param x a numeric vector already shifted to positive values (see 
+#' [find_shift_factor()]). This function requires at least 2 distinct values to 
+#' work.
+#' 
+#' @return 
+#' An integer that can be used to scale `x` to a reasonable range. For binary
+#' variables 1 is returned.
+#' 
+#' @references 
+#' Royston, P., and Sauerbrei, W., 2008. \emph{Multivariable Model - Building: 
+#' A Pragmatic Approach to Regression Anaylsis based on Fractional Polynomials 
+#' for Modelling Continuous Variables. John Wiley & Sons.}
+#' 
 #' @export
 find_scale_factor <- function(x) {
-  # see https://www.stata.com/manuals/rfp.pdf
-  mx <- unique(x)
-  if (length(mx) == 1) stop("all values of x are identical hence log(max(x)-min(x)) = log(0) not allowed")
-  lrange <- log10(max(x, na.rm = T) - min(x, na.rm = T))
-  # scales <- 10^(sign(lrange)*as.integer(lrange))
-  scales <- 10^(sign(lrange) * floor(abs(lrange)))
-  return(scales)
+
+  n_unique = length(unique(x))
+  
+  if (n_unique == 1)
+    stop("! Input data must not be constant.", 
+         "i All values of x are identical, hence log(max(x)-min(x)) = log(0) is not defined.")
+  
+  if (n_unique == 2)
+    return(1)
+    
+  lrange <- log10(max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+  
+  10^(sign(lrange) * floor(abs(lrange)))
 }
 
-# estimates factors for shifting continuous variables to positive values. If x
-# is a two level variable or all values are positive then a factor of 0 is return
-# hence no shifting will be conducted
+#' Function that calculates an integer used to shift predictor
+#' 
+#' @details
+#' For details on why shifting is necessary see the corresponding section in the
+#' documentation of [mfpa()].
+#' 
+#' This function implements the formula in Section 4.7 of Royston and 
+#' Sauerbrei (2008).
+#' 
+#' @param x a numeric vector.
+#' 
+#' @return 
+#' An integer that can be used to shift `x` to positive values. If all values 
+#' are positive, or if `x` is binary then 0 is returned. 
+#' 
+#' @references 
+#' Royston, P., and Sauerbrei, W., 2008. \emph{Multivariable Model - Building: 
+#' A Pragmatic Approach to Regression Anaylsis based on Fractional Polynomials 
+#' for Modelling Continuous Variables. John Wiley & Sons.}
+#' 
+#' @export
 find_shift_factor <- function(x) {
-  nxx <- length(unique(x))
-  if (all(x > 0) || nxx <= 2) {
-    adj.factors <- 0
-  } else {
-    # see section 4.7 in Royston and Sauerbrei (2008) book for the formula
-    difx <- diff(sort(x))
-    # get rid of 0 differences and find the minimum
-    eps <- min(difx[difx != 0])
-    # adjustment factor
-    adj.factors <- eps - min(x, na.rm = T)
-  }
-  return(adj.factors)
+  
+  n_unique <- length(unique(x))
+  
+  if (all(x > 0) || n_unique <= 2) 
+    return(0)
+  
+  difx <- diff(sort(x))
+  eps <- min(difx[difx != 0])
+  
+  eps - min(x, na.rm = TRUE)
 }
 
 #' A function that is used to shift x values to positive values if it contains

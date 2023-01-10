bfb3edd352a40be5b2639cc50e76fa83313ae2fd
Michael Kammer <michael.kammer@mailbox.org>
2023-01-10 11:53:48 +0100

    REF: moved all test computations into single file

--- R/tests.R
+++ R/tests.R
@@ -1,3 +1,49 @@
+#' Function to calculate p-values for Chi-square distribution
+#' 
+#' @param dev a vector of deviance for models i.e Null, linear, FP1,...FPm.
+#' @param acd logical indicating the use of acd transformation.
+calculate_chisquare_test <- function(dev, 
+                                     acd) {
+  if (acd) {
+    df <- c(4, 3, 2, 2, 1)
+    # we have dev = (NULL, M4, M2, M3, M1, M5)
+    # calculate p-values for the functions selection procedure:
+    # a). M1 vs Null     b). M1 vs M4     c). M1 vs M2    d) M1 vs M3
+    pvalues <- dev.diff <- numeric(5)
+    for (i in 1:4) {
+      dev.diff[i] <- dev[i] - dev[5]
+      pvalues[i] <- pchisq(dev.diff[i], df = df[i], lower.tail = F)
+    }
+    # e). M3 vs M5
+    dev.diff[5] <- dev[6] - dev[4]
+    pvalues[5] <- pchisq(dev.diff[5], df = df[5], lower.tail = F)
+  } else {
+    # we have dev = c(NULL, Linear, FP1, FP2,....FPm)
+    nn <- length(dev)
+    # Maximum permitted degree: nn-2, the first and second position is null and linear
+    m <- nn - 2
+    # calculate degrees of freedom for testing. Note that:
+    # FPm vs Null = 2m; FPm vs linear = 2m-1; FPm vs FP1 = 2m-2; FPm vs FP2 = 2m-4
+    # in general: FPm vs FPk = 2(m-k) for m>k
+    k <- seq(m - 1, 1)
+    df <- c(2 * m, 2 * m - 1, if (m >= 2) {
+      2 * (k)
+    }) # same as 2*(m-k) where k = seq(1,m-1)
+    # calculate p-values for the functions selection procedure:
+    # FPm vs Null; FPm vs linear; FPm vs FP1; FPm vs FP2 etc.
+    pvalues <- dev.diff <- numeric(nn - 1)
+    for (i in 1:(nn - 1)) {
+      dev.diff[i] <- dev[i] - dev[nn]
+      pvalues[i] <- pchisq(dev.diff[i], df = df[i], lower.tail = F)
+    }
+  }
+  
+  list(
+    pvalues = pvalues, 
+    dev.diff = dev.diff
+  )
+}
+
 #' Function to compute F-statistic and p-value
 #' 
 #' @details 

d0128ec62aaf2a15a7260f1805ec37a38e79e850
Edwin Kipruto <kipruto@ad.uniklinik-freiburg.de>
2023-04-25 13:18:42 +0200

    Exported the function to be used in predict.mfpa()

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -81,17 +81,19 @@ find_shift_factor <- function(x) {
 
 #' A function that is used to shift x values to positive values if it contains
 #' negative or zero values.If all values of x are positive then the original
-#' values of x is returned without shifting.
+#' values of x is returned without shifting .
 #' if x has already been shifted and scaled then the function does nothing
 #' 
 #' @param x A vector of predictor variable
 #' @param scale scaling factors for x of interest. Must be positive integers.
 #' Default is NULL and  scaling factors are automatically estimated using
-#' find_scale_factor() function else it uses user suppled scaling factors. If no scaling
-#' is needed just use scale = 1 because x/1 = x
+#' find_scale_factor() function else it uses user supplied scaling factors. If no scaling
+#' is needed just use scale = 1
 #' @param shift adjustment factors required for shifting x to positive
 #' values. Default is NULL and adjustment factors are estimated automatically
-#' using Royston and Sauerbrei formula.
+#' using find_shift_factor() function
+#' 
+#' @export
 apply_shift_scale <- function(x, scale, shift) {
   # restrict x to be a vector not matrix
   if (is.matrix(x)) stop("x must be a vector not a matrix")

10e384fa10cc8f3388a8fc96efc111d38d738159
Michael Kammer <michael.kammer@mailbox.org>
2023-04-28 10:15:30 +0200

    REF: refactored code for predict.mfpa

--- R/predict.mfpa.R
+++ R/predict.mfpa.R
@@ -11,6 +11,9 @@
 #'fractional polynomial (FP) power. Once the transformation is complete, the 
 #'transformed data is passed to either `predict.glm()` or `predict.coxph()`, 
 #'depending on the chosen family of models.
+#'
+#' TODO: document behaviour of terms, if terms for glm are desired use
+#' predict.glm directly...
 #' 
 #'@param object a fitted object of class `mfpa` which inherit from `glm`,`lm` or `coxph`.
 #'@param newdata optionally, a data frame in which to look for variables with
@@ -29,76 +32,84 @@
 predict.mfpa <- function(object, 
                          newdata=NULL, 
                          type = NULL,
-                         terms = NULL, 
+                         terms = NULL,
                          strata = NULL, 
                          offset = NULL, 
                          ...) {
   print("hello")
-  if (is.null(type)) type <- ifelse(object$family_string == "cox", "lp", "link")
+  if (is.null(type)) 
+    type <- ifelse(object$family_string == "cox", "lp", "link")
   
   # TODO: add checks for missing strata and offset in case they were used in fit
   
-  # Transform newdata using the FP powers from the training model
+  if (type == "terms") {
+    
+  }
+  
+  # transform newdata using the FP powers from the training model
   if (!is.null(newdata)) {
-    ## STEP 1: shift and scale data using using shifting and scaling factors from the training data
-     shift <- object$transformations[,"shift"]
-     scale <- object$transformations[,"scale"]
-    #subset and sort columns of newdata based on the names of shift/scale
+    # step 1: shift and scale data using using shifting and scaling factors 
+    # from the training data
+    # 
+    # subset and sort columns of newdata based on the names of shift/scale
     newdata <- newdata[, rownames(object$transformations), drop = FALSE]
-    # shift and scale
-    newdata_shifted <- sweep(newdata, 2, shift, "+")
-    if (!all(newdata_shifted > 0)) 
+    newdata <- sweep(newdata, 2, object$transformations[,"shift"], "+")
+    
+    if (!all(newdata > 0)) 
       warning("i After shifting using training data some values in newdata remain negative.",
               "i Predictions for such observations may not be available in case of non-linear transformations.")
     
-    newdata_shifted_scaled <- sweep(newdata_shifted, 2, scale, "/")
+    newdata <- sweep(newdata, 2, object$transformations[,"scale"], "/")
     
-    ## STEP 2: TRANSFORM THE SHIFTED AND SCALED DATA
-    # Set names to center and acd required by transform_matix()
-    center_varx <- setNames(rep(FALSE, nrow(object$transformations)), 
-                            rownames(object$transformations))
-    acd_varx <- setNames(object$fp_terms[,"acd"], 
-                         rownames(object$fp_terms))
-    
-    # Apply transformating to the newdata
+    # step 2: transform the shifted and scaled data
+    # do not center in this step
     newdata_transformed <- transform_matrix(
-        newdata_shifted_scaled,
-        power_list = object$fp_powers, 
-        center = center_varx,
-        keep_x_order = T,
-        acdx = acd_varx
+      newdata,
+      power_list = object$fp_powers, 
+      center = setNames(rep(FALSE, nrow(object$transformations)), 
+                        rownames(object$transformations)),
+      keep_x_order = TRUE,
+      acdx = setNames(object$fp_terms[,"acd"], 
+                      rownames(object$fp_terms))
     )
     
-    ## STEP 3: CENTER THE TRANSFORMED DATA
+    # step 3: center the transformed data
     if (!is.null(object$centers)) {
-      newdata_transformed <- center_matrix(
+      newdata <- center_matrix(
         newdata_transformed$x_transformed,
         object$centers
       )
     }
     
-    newdata_transformed <- data.frame(newdata_transformed)
+    newdata <- data.frame(newdata)
     
     if (object$family_string == "cox") {
       # use of NextMethod here does not work as expected
       
       # add strata and offset as required
       if (!is.null(strata))
-        newdata_transformed$strata_ = survival::strata(strata, shortlabel = TRUE)
+        newdata$strata_ = survival::strata(strata, shortlabel = TRUE)
       
       if (!is.null(offset))
-        newdata_transformed$offset_ = offset
+        newdata$offset_ = offset
       
-      return(getFromNamespace("predict.coxph", "survival")(object = object, newdata = newdata_transformed, type = type, ...))
+      # use getFromNamespace here, as predict.coxph is not exported
+      return(getFromNamespace("predict.coxph", "survival")(
+        object = object, newdata = newdata, type = type, ...
+      ))
     } else {
       # TODO: offsets for glm?
-      return(stats::predict.glm(object = object, newdata = newdata_transformed, type = type, ...))
+      return(stats::predict.glm(
+        object = object, newdata = newdata, type = type, ...
+      ))
     } 
   }
   
   # no newdata supplied
   if (object$family_string == "cox") {
-    getFromNamespace("predict.coxph", "survival")(object = object, type = type, ...)
+    getFromNamespace("predict.coxph", "survival")(
+      object = object, type = type, ...
+    )
   } else {
     stats::predict.glm(object = object, type = type, ...)
   }

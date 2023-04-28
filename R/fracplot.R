500a7a3b4b4e218d6122c68b22785697583c6215
Michael Kammer <michael.kammer@mailbox.org>
2023-04-28 15:59:12 +0200

    ADD: parameters for fracplot

--- R/fracplot.R
+++ R/fracplot.R
@@ -1,59 +1,50 @@
-#' @title plot data and fit from fractional polynomial model
+#' Plot response functions from a fitted `mfpa` object
 #'
-#' @description plots the data and fit, with 95% confidence limits, from the fractional
-#' polynomial (FP) model. The data and fit are plotted against the selected covariate of
-#' interest.
+#' Plots partial linear predictors against the selected covariate(s) of 
+#' interest, including a confidence interval.
 #'
-
-#' @param model mfpa model
-#' @param x  variable name to be plotted.
-#' @param plotype selects which graphics engine to use to generate the
-#' fractional polynomial plot. Either 'ggplot' or 'rplot'. Default is "ggplot"
-#' @param partials produces component-plus-residual plots of the continuous variables when set to TRUE otherwise
-#' partial predictor would be produced. Default is TRUE
-#' @param col.points The color of the data points when component-plus-residual plot is generated
-#' @param col.line The color of the partial predictor line
-#' @param title The title of the plot
-#' @param size,cex line width of the partial predictor and the size of the points
-#' when component-plus-residual plot is generated
-#' @param ylab,xlab,pch other graphical parameters
-#' @param fill the color for confidence interval
-#' @param alpha refers to the opacity of the confidence interval.
-#' Values of alpha range from 0 to 1, with lower values corresponding to more
-#' transparent colors. See ggplot2
-
-#' ## Details on `plotype` option
-#'
-#' Selects which graphics engine to use to generate the
-#' fractional polynomial plot. Either 'ggplot' or 'rplot'. Default is "ggplot"
-#'
-#' For Cox models, the response should preferably be a \code{Surv} object,
-#' created by the \code{Surv()} function in \pkg{survival} package and the \code{family = cox}. Only
-#' right-censored data are currently supported. To fit stratified Cox
-#' models, strata option can be used.
-
-
-## @method plot mfpa
-#' @import graphics
+#' @param model fitted `mfpa` model.
+#' @param terms character vector with variable names to be plotted.
+#' @param partial_only a logical value indicating whether only the partial 
+#' predictor is drawn (`TRUE`), or also observed data points (`FALSE`, the 
+#' default). See below for details. 
+#' @param terms_seq `terms_seq` argument of [predict.mfpa()].
+#' @param terms_alpha `terms_alpha` argument of [predict.mfpa()].
+#' @param shape,size_points,color_points `ggplot2` properties of drawn 
+#' data points.
+#' @param color_line,linetype,linewidth `ggplot2` properties of line for 
+#' partial predictor.
+#' @param color_fill,alpha_fill `ggplot2` properties of ribbon for confidence
+#' interval.
+#' 
+#' @details 
+#' The data points are computed as the partial linear predictors plus residuals
+#' extracted from the fitted model (deviance residuals for glms, and martingale
+#' residuals for Cox models), following the mfp implementation in Stata.
+#' 
+#' @return 
+#' A list of `ggplot2` plot objects, one for each term requested. Can be 
+#' drawn as individual plots of facetted / combined easily using e.g. 
+#' `patchwork::wrap_plots` and further customized. 
+#' 
+#' @seealso 
+#' [predict.mfpa()]
+#' 
 #' @import ggplot2
 #' @export
 fracplot <- function(model, 
                      terms = NULL, 
                      partial_only = FALSE, 
                      terms_seq = "data",
-                     col.points = "gray",
-                     title = NULL, 
-                     ylim = NULL,
-                     col.line = "red", 
-                     pch = 16, 
-                     size = 2, 
-                     lty = 1, 
-                     cex = 1, 
-                     ylab = NULL, 
-                     xlab = NULL, 
-                     fill = "gray",
-                     alpha = 0.3, 
-                     ...) {
+                     terms_alpha = 0.05,
+                     color_points = "#AAAAAA",
+                     color_line = "#000000", 
+                     color_fill = "#000000",
+                     shape = 1,
+                     size_points = 1,
+                     linetype = "solid", 
+                     linewidth = 1,
+                     alpha_fill = 0.1) {
   
   # Does a check if ggplot2 is available
   # It should be as it is in the imports section but in CRAN checks some systems don't have it!
@@ -92,9 +83,11 @@ fracplot <- function(model,
   for (v in names(pred)) {
     plots[[v]] <- ggplot2::ggplot(data = pred[[v]], 
                          aes(x = variable, y = contrast)) + 
-      ggplot2::geom_line() + 
+      ggplot2::geom_line(linewidth = linewidth,
+                         linetype = linetype,
+                         color = color_line) + 
       ggplot2::geom_ribbon(aes(ymin = lower, ymax = upper), 
-                  alpha = 0.1) + 
+                           alpha = alpha_fill) + 
       ggplot2::ggtitle(sprintf("FP(%s)%s", 
                       paste0(model$fp_powers[[v]], collapse = ", "),
                       ifelse(model$fp_terms[v, "acd"], ", ACD", "")
@@ -104,13 +97,16 @@ fracplot <- function(model,
     
     if (!partial_only) 
       plots[[v]] <- plots[[v]] + 
-        ggplot2::geom_point(data = pred_data[[v]], aes(y = contrast + resid))
+        ggplot2::geom_point(data = pred_data[[v]], aes(y = contrast + resid),
+                            color = color_points,
+                            size = size_points, 
+                            shape = shape)
   }
   
   plots
 }
 
-# alias to keep things from breaking
+#' @describeIn fracplot Alias for fracplot.
 plot_mfp <- function(...) {
     fracplot(...)
 }

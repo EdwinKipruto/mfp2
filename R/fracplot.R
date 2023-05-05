d360f0d90dbdde0a33510dac27efc750d07c9865
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2023-05-05 09:56:15 +0200

    Fixed default reference in fracplot
    I think I found a bug in `fracplot()`. If `ref = NULL`, I would always get the "ref must be a list" error. But I think I should only get this error if I specified my own reference value not as a list.

--- R/fracplot.R
+++ R/fracplot.R
@@ -66,8 +66,8 @@ fracplot <- function(model,
   if (!inherits(model, "mfp2")) 
     stop("The model entered is not an mfp2 object.", call. = FALSE)
   
- # check if ref is a list 
-  if (!is.list(ref))
+ # check if ref is a list (only if it is not NULL)
+  if (!is.null(ref) && !is.list(ref))
     stop("ref must be a list", call. = F)
   
  # set defaults depending on type

851cd87fb4f2b11a430327cb964507d8b4960ca3
Edwin Kipruto <kipruto@ad.uniklinik-freiburg.de>
2023-05-05 15:39:08 +0200

    ADDED: acd argument

---
+++ R/fp.R
@@ -0,0 +1,19 @@
+#' Helper function to assign attribute to a variable
+#' 
+#' @return 
+#' a variable with its attributes.
+#' 
+#' @export
+fp <- function(x, df = 4, select = NA, alpha = NA, scale=TRUE, acd = FALSE)
+{
+  #
+  name <- deparse(substitute(x))
+  attr(x, "df") <- df
+  attr(x, "alpha") <- alpha
+  attr(x, "select") <- select
+  attr(x, "scale") <- scale
+  attr(x, "acd") <- acd
+  attr(x, "name") <- name
+  x
+}
+

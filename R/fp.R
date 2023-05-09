ba4a254d9d2b7f37a3410585cc6360e7bc5fcb7a
Edwin Kipruto <kipruto@ad.uniklinik-freiburg.de>
2023-05-09 18:50:14 +0200

    ADDED: added a condition that checks whether x        is a factor variable

--- R/fp.R
+++ R/fp.R
@@ -1,6 +1,6 @@
-#' A function that assign attribute to a variable that undergo fp transformation
+#' A function that assign attribute to a continuous variable that undergo fp transformation
 #' 
-#' @param x a vector of variable to be assigned attributes
+#' @param x a vector of continuous variable to be assigned attributes
 #' @param df,alpha,select,shift,scale,center,acd See [mfp2::mfp2()]) for details. 
 #' @return 
 #' a variable with its attributes.
@@ -8,7 +8,11 @@
 #' @export
 fp <- function(x, df = 4, alpha = 0.05,select = 0.05, shift = NULL, scale=NULL,center = TRUE, acd = FALSE)
 {
+
   name <- deparse(substitute(x))
+  # Assert that a factor variable must not be subjected to fp transformation
+  if(is.factor(x))
+    stop(name," is a factor variable and must not undergo fp transformation")
   # attributes of x
   attr(x, "df") <- df
   attr(x, "alpha") <- alpha

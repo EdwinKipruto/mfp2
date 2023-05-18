52aba648f79de096021f357ffbd333fd992ae698
Edwin Kipruto <kipruto@ad.uniklinik-freiburg.de>
2023-05-18 13:58:16 +0200

    ADDED: documentation

--- R/fp.R
+++ R/fp.R
@@ -2,6 +2,30 @@
 #' 
 #' @param x a vector of continuous variable to be assigned attributes
 #' @param df,alpha,select,shift,scale,center,acd See [mfp2::mfp2()]) for details. 
+#' @param scale a numeric vector of length nvars or single numeric specifying 
+#' scaling factors. If a single numeric, then the value will be replicated as
+#' necessary. Default is `NULL` which lets the program estimate the scaling 
+#' factors (see Details section).
+#' If scaling is not required set `scale = 1` to disable it.
+#' @param shift a numeric vector of length nvars or a single numeric specifying
+#' shift terms. If a single numeric, then the value will be replicated as
+#' necessary. Default is `NULL` which lets the program estimate the shifts
+#' (see Details section).
+#' If shifting is not required, set `shift = 0` to disable it.
+#' @param df a numeric vector of length nvars or a single numeric that sets the 
+#' (default) degrees of freedom (df) for each predictor. If a single numeric, 
+#' then the value will be replicated as necessary. The df (not counting 
+#' the intercept) are twice the degree of a fractional polynomial (FP). 
+#' For example, an FP2 has 4 df, while FP3 has 6 df. 
+#' The program overrides default df based on the number of distinct (unique) 
+#' values for a variable as follows: 
+#' 2-3 distinct values are assigned `df = 1` (linear), 4-5 distinct values are
+#' assigned `df = min(2, default)` and >= 6 distinct values are assigned  
+#' `df = default`. NOT in mfp2.formula()...df = 1 makes sense in formula to avoid warnings from binary variables
+#' @param center a logical determining whether variables are centered before 
+#' model fit. The default `TRUE` implies mean centering, except for binary 
+#' covariates, where the covariate is centered using the lower of the two 
+#' distinct values of the covariate. See Details section below.
 #' @param pow a vector of powers to be evaluated for x. Default is NULL and 
 #' powers = c(-2, -1, -0.5, 0, 0.5, 1, 2, 3) will be used
 #' @return 

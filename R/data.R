efc7bd30d7cfff3d30cd7600f3532416f6eb3641
EdwinKipruto <kiprutohedwin@gmail.com>
2023-09-19 16:50:02 +0200

    Data documentation

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Artificial dataset with continuous response
 #'
-#' The ART data set mimic the GBSG breast cancer study in terms of the 
-#' distribution of predictors and correlation structure
+#' The ART data set mimics the GBSG breast cancer study in terms of the 
+#' distribution of predictors and correlation structure.
 #'
 #' @name art
 #' @docType data
@@ -9,80 +9,82 @@
 #' @keywords data
 #' @format The dataset has 250 observations and 10 covariates
 #' \describe{
-#'   \item{y}{continuous response variable.}
-#'   \item{x1, x3, x5-x7, x10}{continous covariates.}
-#'   \item{x2}{binary variable.}
-#'   \item{x4}{ordinal variable with 3 levels.}
-#'   \item{x8}{binary variable.}
-#'   \item{x9}{nominal variable with 3 levels}
+#'   \item{y}{Continuous response variable.}
+#'   \item{x1, x3, x5-x7, x10}{Continuous covariates.}
+#'   \item{x2}{Binary variable.}
+#'   \item{x4}{Ordinal variable with 3 levels.}
+#'   \item{x8}{Binary variable.}
+#'   \item{x9}{Nominal variable with 3 levels.}
 #' }
 "art"
 
-#' Breast cancer data set used in Royston and Sauerbrei (2008) book
+#' Breast cancer dataset used in the Royston and Sauerbrei (2008) book.
 #'
 #' @name gbsg
 #' @docType data
 #' @usage data(gbsg)
 #' @keywords data
-#' @format A dataset with 686 observations and 11 variables
+#' @format A dataset with 686 observations and 11 variables.
 #' \describe{
-#'   \item{id}{patient identifier.}
-#'   \item{age}{age in years.}
-#'   \item{meno}{menopausal status (0 = premeno, 1 = postmeno).}
-#'   \item{size}{tumor size(mm)}
-#'   \item{grade}{tummor grade}
-#'   \item{nodes}{number of positive lymph nodes}
-#'   \item{enodes}{exp(-0.12*nodes)}
-#'   \item{pgr}{progestrone receptor status}
-#'   \item{er}{oestrogen receptor status}
-#'   \item{hormon}{tamoxifen treatment}
-#'   \item{rectime}{time(days) to death or cancer recurrence}
-#'   \item{censrec}{censoring(0 = censored, 1 = event)}
+#'   \item{id}{Patient identifier.}
+#'   \item{age}{Age in years.}
+#'   \item{meno}{Menopausal status (0 = premeno, 1 = postmeno).}
+#'   \item{size}{Tumor size (mm).}
+#'   \item{grade}{Tumor grade.}
+#'   \item{nodes}{Number of positive lymph nodes.}
+#'   \item{enodes}{exp(-0.12*nodes).}
+#'   \item{pgr}{Progesterone receptor status.}
+#'   \item{er}{Estrogen receptor status.}
+#'   \item{hormon}{Tamoxifen treatment.}
+#'   \item{rectime}{Time (days) to death or cancer recurrence.}
+#'   \item{censrec}{Censoring (0 = censored, 1 = event).}
 #' }
 "gbsg"
 
-#' Pima Indians dataset used in Royston and Sauerbrei (2008) book
+
+#' Pima Indians dataset used in the Royston and Sauerbrei (2008) book.
 #'
 #' The dataset arises from an investigation of potential predictors of
 #' the onset of diabetes in a cohort of 768 female Pima Indians of whom 268
 #' developed diabetes. Missing values were imputed using the ice procedure
-#' for stata
+#' for Stata.
 #'
 #' @name pima
 #' @docType data
 #' @usage data(pima)
 #' @keywords data
-#' @format A data set with 768 observations and 9 variables.
+#' @format A dataset with 768 observations and 9 variables.
 #' \describe{
-#'   \item{id}{patient identifier.}
-#'   \item{pregnant}{number of times pregnant.}
-#'   \item{glucose}{plasma glucose concentration at 2h in an oral glucose tolerance test.}
-#'   \item{diastolic}{diastolic blood pressure in mmHg}
-#'   \item{triceps}{triceps skin fold thickness in mm}
-#'   \item{insulin}{2-h serum insulin}
-#'   \item{bmi}{body mass index}
-#'   \item{diabetes}{diabetes pedigree function}
-#'   \item{age}{age in years}
-#'   \item{y}{binary outcome variable (diabetes, yes/no)}
+#'   \item{id}{Patient identifier.}
+#'   \item{pregnant}{Number of times pregnant.}
+#'   \item{glucose}{Plasma glucose concentration at 2h in an oral glucose tolerance test.}
+#'   \item{diastolic}{Diastolic blood pressure in mmHg.}
+#'   \item{triceps}{Triceps skin fold thickness in mm.}
+#'   \item{insulin}{2-h serum insulin.}
+#'   \item{bmi}{Body mass index.}
+#'   \item{diabetes}{Diabetes pedigree function.}
+#'   \item{age}{Age in years.}
+#'   \item{y}{Binary outcome variable (diabetes, yes/no).}
 #' }
 "pima"
 
-#' Prostate cancer dataset used in Royston and Sauerbrei (2008)
+
+#' Prostate cancer dataset used in the Royston and Sauerbrei (2008) book.
 #'
 #' @name prostate
 #' @docType data
 #' @usage data(prostate)
 #' @keywords data
-#' @format A data set with 97 observations and 8 variables.
+#' @format A dataset with 97 observations and 8 variables.
 #' \describe{
-#'   \item{obsno}{observation number.}
-#'   \item{age}{age in years.}
-#'   \item{svi}{seminal vessel invasion (yes/no)}
-#'   \item{pgg45}{percentage gleason score 4 or 5}
-#'   \item{cavol}{cancer volume (mm)}
-#'   \item{weight}{prostate weight (g)}
-#'   \item{bph}{amount of benign prostatic hyperplasia (g)}
-#'   \item{cp}{amount of capsular penetration (g)}
-#'   \item{lpsa}{log PSA concentration-outcome variable}
+#'   \item{obsno}{Observation number.}
+#'   \item{age}{Age in years.}
+#'   \item{svi}{Seminal vessel invasion (yes/no).}
+#'   \item{pgg45}{Percentage Gleason score 4 or 5.}
+#'   \item{cavol}{Cancer volume (mm).}
+#'   \item{weight}{Prostate weight (g).}
+#'   \item{bph}{Amount of benign prostatic hyperplasia (g).}
+#'   \item{cp}{Amount of capsular penetration (g).}
+#'   \item{lpsa}{Log PSA concentration (outcome variable).}
 #' }
 "prostate"
